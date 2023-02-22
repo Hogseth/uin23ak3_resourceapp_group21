42891509bf678a70be8d891ca91d2d6a735b6bcd
Sperlini <Alexander.sperlin@hotmail.com>
2023-02-22 14:09:14 +0100

    la til class paa buttons.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,12 @@
 //import resources from './Ressurser.js'
 export default function Button(){
     return (
-        <>
+        <div className="buttons">
         <button>placeholder</button>
         <button>placeholder</button>
         <button>placeholder</button>
         <button>placeholder</button>
         <button>placeholder</button>
-        </>
+        </div>
     )
 }
