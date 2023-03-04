7a981f229a85d96662d87843af481bd24e0ce236
AndreStaalesen <74905737+AndreStaalesen@users.noreply.github.com>
2023-03-04 16:14:17 +0100

    test slug routing start

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,3 +1,17 @@
-export default function Article(){
-    return
+import { useParams } from "react-router-dom"
+
+export default function Article({resources}){
+    const {slug} = useParams()
+    const resource = recources.find((rec) => rec.slug === slug)
+
+    return(
+        <section>
+            <h1>{resource.category}</h1>
+            <ul>
+                {resource.url.map((url, index) =>(
+                <li key={"url"+index}>{url}</li>
+            ))}
+            </ul>
+        </section>
+    )
 }
